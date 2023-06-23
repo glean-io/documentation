848cb282dfacd5f66c7655a1a4923cecb3850ead
Charlie Imhoff <charlie@glean.io>
2023-06-23 16:54:35 -0400

    Fix middleware loop with case sensitive files

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === request.nextUrl.pathname.toLocaleLowerCase())
+  // never redirect requests against files with extensions, as those include
+  // hashes that may be case sensitive
+  const pathComponents = request.nextUrl.pathname.split("/");
+  const lastPathComponent = pathComponents[pathComponents.length - 1];
+  if (lastPathComponent.includes(".")) return NextResponse.next();
+
+  // Otherwise, redirect to the same request with a lowercase path
+  if (request.nextUrl.pathname === request.nextUrl.pathname.toLowerCase())
     return NextResponse.next();
   return NextResponse.redirect(
-    `${request.nextUrl.origin}${request.nextUrl.pathname.toLocaleLowerCase()}`
+    `${request.nextUrl.origin}${request.nextUrl.pathname.toLowerCase()}`
   );
 }
