442f636b8d671c7fab5d34360f2ad9e84c2f6d62
Andrew Lee <hello@vndrewlee.com>
2023-07-05 14:49:32 -0400

    init branch

---
+++ components/Iframe.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { HTMLAttributes } from "react";
+
+type IframeProps = { src: string } & HTMLAttributes<HTMLIFrameElement>;
+
+export default function Iframe(props: IframeProps) {
+  return <iframe width="100%" height="500px" {...props} />;
+}
