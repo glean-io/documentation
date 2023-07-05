e8cdc986341fcdc6bc2c1bc5e14502f62f016c68
Andrew Lee <hello@vndrewlee.com>
2023-07-05 14:52:35 -0400

    remove border

--- components/Iframe.tsx
+++ components/Iframe.tsx
@@ -5,5 +5,7 @@ import React, { HTMLAttributes } from "react";
 type IframeProps = { src: string } & HTMLAttributes<HTMLIFrameElement>;
 
 export default function Iframe(props: IframeProps) {
-  return <iframe width="100%" height="500px" {...props} />;
+  return (
+    <iframe width="100%" height="500px" style={{ border: "none" }} {...props} />
+  );
 }
